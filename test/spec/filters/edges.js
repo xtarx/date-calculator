07e876396b6ae32295471a6ad2ba2d5ffe602e6d
xtarx <xtarx0@gmail.com>
2015-07-30 17:54:49 +0200

    update

---
+++ test/spec/filters/edges.js
@@ -0,0 +1,33 @@
+// 'use strict';
+
+// describe('EdgesFilter', function () {
+
+//   var filter;
+
+//   // load the controller's module
+//   beforeEach(module('swFrontApp'));
+
+//   // Initialize the controller and a mock scope
+//   beforeEach(inject(function (edgesFilter) {
+//    filter=edgesFilter;
+//  }));
+
+//   desribe('category filter',function  () {
+//     var edges=[   
+//     {
+//       category:{name:'first'},
+//     },
+//     {
+//       category:{name:'second'},
+//     },
+
+//     ];
+
+//     var filterBy={category:{name:'first'}};
+//     it('filter by category name', function () {
+//       expect(filter(edges,filterBy).length).toEqual(1);
+//     });
+
+//   })
+
+// });
